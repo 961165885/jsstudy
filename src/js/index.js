d45aa29f09860f3fb15b76e648f7a723ac2a99e8
961165885 <jhh961165885@outlook.com>
2021-08-28 11:44:17 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,16 @@ const container = document.querySelector('#root');
 const mainArea = document.createElement('div');
 mainArea.setAttribute('id', 'main-area');
 
+const nextArea = document.createElement('div');
+nextArea.setAttribute('id', 'prev-area');
+
 const width = 10;
 const height = 15;
 
 const grids = [];
+
+grids.timer = null;
+grids.item = null;
 
 for (let x = 0; x < height; x++) {
     grids[x] = [];
@@ -20,6 +26,7 @@ for (let x = 0; x < height; x++) {
 
         grids[x][y] = {
             el: temp,
+            isFilled: false,
             addClass: function(cls) {
                 temp.classList.add(cls);
             },
@@ -28,6 +35,50 @@ for (let x = 0; x < height; x++) {
             }
         };
     }
+}
+
+const nextGrid = [];
+nextGrid.item = null;
+
+for (let x = 0; x < 4; x++) {
+    nextGrid[x] = [];
+    for (let y = 0; y < 4; y++) {
+        const temp = document.createElement('div');
+        nextArea.appendChild(temp);
+
+        temp.classList.add(`grid-item`);
+        temp.classList.add(`grid-item-row-${x}`);
+        temp.classList.add(`grid-item-column-${y}`);
+
+        nextGrid[x][y] = {
+            el: temp,
+            addClass: function(cls) {
+                temp.classList.add(cls);
+            },
+            removeClass: function(cls) {
+                temp.classList.remove(cls);
+            }
+        };
+    }
+}
+
+
+nextGrid.addItem = function(item) {
+    if (this.item) {
+        this.item.forEach(pos => {
+            this[pos[1]][pos[0]].removeClass('active');
+        });
+    }
+
+    this.item = item;
+
+    this.showItem();
+}
+
+nextGrid.showItem = function() {
+    this.item.forEach(pos => {
+        this[pos[1]][pos[0]].addClass('active');
+    });
 }
 
 const itemPool = [
@@ -93,52 +144,146 @@ const itemPool = [
             [0, 1],
         ],
     ],
+    [
+        [
+            [1, 0],
+            [0, 1],
+            [1, 1],
+            [2, 1],
+        ],
+        [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [0, 2],
+        ],
+        [
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [1, 1],
+        ],
+        [
+            [0, 1],
+            [1, 0],
+            [1, 1],
+            [1, 2],
+        ],
+    ]
 ];
 
-grids.addItem = function(item) {
-    item.forEach(pos => {
-        this[pos[1]][pos[0]].addClass('active');
-    });
+grids.createItem = function() {
+    let item;
+    if (nextGrid.item) {
+        item = nextGrid.item;
+    } else {
+        item = generateItem();
+    }
+
+    const nextItem = generateItem();
+    nextGrid.addItem(nextItem);
+
+    setItemPos(item);
+
+    this.addItem(item);
 }
 
-grids.renderItem = function(item) {
+grids.addItem = function(item) {
+    this.item = item;
+
+    this.renderItem();
+}
+grids.showItem = function() {
+    this.item.forEach(pos => {
+        this[pos[1]][pos[0]].addClass('active');
+    });
+};
+
+grids.onItemSet = function() {
+    if (this.timer) {
+        clearInterval(this.timer);
+    }
+
+    this.item.forEach(pos => {
+        this[pos[1]][pos[0]].isFilled = true;
+    });
+
+    this.createItem();
+}
+
+grids.calItemDown = function(item) {
+    let isSet = false;
+
+    let tempItem = [];
     item.forEach(pos => {
-        pos[1]++;
+        const newPos = [pos[0], pos[1] + 1];
 
         if (pos[1] == 14) {
-            clearInterval(timer);
+            isSet = true;
+        } else if (this[newPos[1]][newPos[0]].isFilled) {
+            isSet = true;
+        }
+
+        tempItem.push(newPos);
+    });
+
+    if (isSet) {
+        return false;
+    }
+
+    return tempItem;
+}
+
+grids.calItemLeft = function(item) {
+
+}
+
+grids.calItemRight = function(item) {
+
+}
+
+grids.renderItem = function() {
+    this.item.forEach(pos => {
+        if (pos[1] == 14) {
+            this.onItemSet();
         }
     });
 
-    this.addItem(item);
+    this.showItem();
 
-    let timer = setInterval(() => {
+    this.timer = setInterval(() => {
         let maxY = 0;
-        item.forEach(pos => {
+        this.item.forEach(pos => {
             if (pos[1] > maxY) {
                 maxY = pos[1];
             }
         });
         if (maxY == 14) {
+            this.onItemSet();
             return;
         }
 
-        item.forEach(pos => {
+        this.item.forEach(pos => {
             this[pos[1]][pos[0]].removeClass('active');
         });
 
-        item.forEach(pos => {
-            pos[1]++;
+        const tempItem = this.calItemDown(this.item);
 
-            if (pos[1] == 14) {
-                clearInterval(timer);
-            }
-        });
+        if (!tempItem) {
+            this.showItem();
+            this.onItemSet();
 
-        this.addItem(item);
-    }, 1000);
+            return;
+        }
+
+        this.item = tempItem;
+
+        this.showItem();
+    }, 500);
 };
-grids.moveLeft = function(item) {
+grids.moveLeft = function() {
+    const item = this.item;
+
     let minX = 9;
     item.forEach(pos => {
         if (pos[0] < minX) {
@@ -157,9 +302,11 @@ grids.moveLeft = function(item) {
         pos[0]--;
     });
 
-    this.addItem(item);
+    this.showItem();
 }
-grids.moveRight = function(item) {
+grids.moveRight = function() {
+    const item = this.item;
+
     let maxX = 0;
     item.forEach(pos => {
         if (pos[0] > maxX) {
@@ -178,11 +325,11 @@ grids.moveRight = function(item) {
         pos[0]++;
     });
 
-    this.addItem(item);
+    this.showItem();
 }
-grids.moveDown = function(item) {
+grids.moveDown = function() {
     let maxY = 0;
-    item.forEach(pos => {
+    this.item.forEach(pos => {
         if (pos[1] > maxY) {
             maxY = pos[1];
         }
@@ -191,17 +338,19 @@ grids.moveDown = function(item) {
         return;
     }
 
-    const dy = 14-maxY;
-
-    item.forEach(pos => {
+    this.item.forEach(pos => {
         this[pos[1]][pos[0]].removeClass('active');
     });
 
-    item.forEach(pos => {
-        pos[1]+=dy;
-    });
+    let tempItem = this.item;
+    let temp;
+    while (temp = this.calItemDown(tempItem)) {
+        tempItem = temp;
+    }
 
-    this.addItem(item);
+    this.item = tempItem;
+
+    this.showItem();
 }
 
 function setItemPos(item) {
@@ -221,25 +370,31 @@ function setItemPos(item) {
     });
 }
 function setItemDirection(itemGroup) {
-    let random = parseInt(Math.random() * itemGroup.length);
-
-    return itemGroup[random];
 }
 function generateItem() {
-    const itemIndex = parseInt(Math.random()*4);
-    const itemGroup = itemPool[itemIndex];
+    const groupIndex = parseInt(Math.random() * itemPool.length);
 
-    const item = setItemDirection(itemGroup);
-    setItemPos(item);
+    const itemIndex = parseInt(Math.random() * itemPool[groupIndex].length);
+
+    const selected = itemPool[groupIndex][itemIndex];
+
+    const item = [];
+
+    selected.forEach(pos => {
+        const temp = [];
+        temp[0] = pos[0];
+        temp[1] = pos[1];
+
+        item.push(temp);
+    });
 
     return item;
 }
 
-const item = generateItem();
-
-grids.renderItem(item);
+grids.createItem();
 
 container.appendChild(mainArea);
+container.appendChild(nextArea);
 
 window.addEventListener('keydown', (e) => {
     switch (e.key) {
@@ -247,14 +402,14 @@ window.addEventListener('keydown', (e) => {
 
             break;
         case 'ArrowDown':
-            grids.moveDown(item);
+            grids.moveDown();
 
             break;
         case 'ArrowLeft':
-            grids.moveLeft(item);
+            grids.moveLeft();
             break;
         case 'ArrowRight':
-            grids.moveRight(item);
+            grids.moveRight();
             break;
 
         default:
